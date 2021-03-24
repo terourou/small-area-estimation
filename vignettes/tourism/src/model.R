e88ff12cdeae55145d89fc125112932e662ee748
johnrbryant <john.r.bryant@gmail.com>
2021-03-25 10:54:50 +1300

    added tourism vignette

---
+++ vignettes/tourism/src/model.R
@@ -0,0 +1,32 @@
+
+library(demest)
+
+guestnights <- readRDS("out/guestnights.rds")
+
+model <- Model(y ~ Poisson(mean ~ region * time,
+                           useExpose = FALSE),
+               time ~ DLM(damp = NULL,
+                          season = Season(n = 12)),
+               region:time ~ DLM(trend = NULL,
+                                 damp = NULL,
+                                 season = Season(n = 12)))
+
+filename <- "out/model.est"
+
+set.seed(0)
+estimateModel(model = model,
+              y = guestnights,
+              filename = filename,
+              nBurnin = 1000,
+              nSim = 1000,
+              nChain = 4,
+              nThin = 5)
+fetchSummary(filename)
+
+
+
+                                  
+                    
+
+
+
